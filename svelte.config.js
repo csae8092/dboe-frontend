f769975481e41848ff0244355f928222298694c2
csae8092 <p.andorfer@gmail.com>
2026-01-17 11:54:09 +0100

    puhh

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,8 @@ const config = {
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
-			strict: false
+			strict: false,
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
