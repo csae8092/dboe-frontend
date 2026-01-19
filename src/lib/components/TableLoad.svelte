a8878283da731337e5db0e0f0f652c1a4d90163c
csae8092 <p.andorfer@gmail.com>
2026-01-19 12:21:10 +0100

    TableLoad and TableNav components

---
+++ src/lib/components/TableLoad.svelte
@@ -0,0 +1,10 @@
+<svelte:options runes={true} />
+
+<script>
+	import { Heading, TextPlaceholder } from 'flowbite-svelte';
+</script>
+
+<div id="placeholder" class="mx-auto flex min-h-[80vh] w-full max-w-7xl flex-col items-center justify-center px-4">
+	<Heading tag="h2">Loading...</Heading>
+	<TextPlaceholder size="xl"></TextPlaceholder>
+</div>
---
+++ src/lib/components/TableNav.svelte
@@ -0,0 +1,30 @@
+<svelte:options runes={true} />
+
+<script>
+	import { P, ButtonGroup, Button, Label, Select } from 'flowbite-svelte';
+	import { ChevronDoubleLeftOutline, ChevronDoubleRightOutline } from 'flowbite-svelte-icons';
+
+	let { pageSize = $bindable(), previousPage, nextPage, handlePageSizeChange, data } = $props();
+</script>
+
+<P class="mb-4">Total count: {data?.count || 0}</P>
+<div class="start mt-4 mb-2 flex">
+	<ButtonGroup>
+		<Button onclick={previousPage} disabled={!data.previous}>
+			<ChevronDoubleLeftOutline class="h-6 w-6" />
+			Previous
+		</Button>
+		<Button onclick={nextPage} disabled={!data.next}>
+			Next
+			<ChevronDoubleRightOutline class="h-6 w-6" />
+		</Button>
+	</ButtonGroup>
+</div>
+<div class="mt-4 mb-4 flex items-center gap-2">
+	<Label for="pageSize">Items per page:</Label>
+	<Select id="pageSize" bind:value={pageSize} onchange={handlePageSizeChange} class="w-24">
+		<option value="10">10</option>
+		<option value="20">20</option>
+		<option value="40">40</option>
+	</Select>
+</div>
