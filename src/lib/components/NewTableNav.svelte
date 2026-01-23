36eaaf5e154ddbf5ee66c54b8e8af5f218578b16
csae8092 <p.andorfer@gmail.com>
2026-01-23 10:32:51 +0100

    loading indicator

--- src/lib/components/NewTableNav.svelte
+++ src/lib/components/NewTableNav.svelte
@@ -1,16 +1,16 @@
 <script>
 	import { Hr, Button, ButtonGroup, P, PaginationNav } from 'flowbite-svelte';
 	import { ChevronDoubleLeftOutline, ChevronDoubleRightOutline } from 'flowbite-svelte-icons';
-	let { data } = $props();
+	let { data, loading = false } = $props();
 </script>
 
 <ButtonGroup>
-	<Button href={data.previous_page} disabled={!data.previous_page}>
+	<Button href={data.previous_page} disabled={!data.previous_page || loading}>
 		<ChevronDoubleLeftOutline class="h-6 w-6" />
 		Vorige Seite
 	</Button>
 
-	<Button href={data.next_page} disabled={!data.next_page}>
+	<Button href={data.next_page} disabled={!data.next_page || loading}>
 		Nächste Seite
 		<ChevronDoubleRightOutline class="h-6 w-6" />
 	</Button>
