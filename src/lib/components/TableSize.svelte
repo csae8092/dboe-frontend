3713e20ccf83a10073f964889b1903b730a84d0f
csae8092 <p.andorfer@gmail.com>
2026-01-23 10:07:10 +0100

    page size select as component

---
+++ src/lib/components/TableSize.svelte
@@ -0,0 +1,20 @@
+<svelte:options runes={true} />
+
+<script>
+	import { Label, Select } from 'flowbite-svelte';
+	import { goto } from '$app/navigation';
+
+	function handlePageSizeChange(event) {
+		const newSize = event.target.value;
+		const url = new URL(window.location.href);
+		url.searchParams.set('page_size', newSize);
+		goto(url.pathname + url.search);
+	}
+</script>
+
+<Label for="page_size">Items per page:</Label>
+<Select id="page_size" onchange={handlePageSizeChange} class="w-24">
+	<option value="10">10</option>
+	<option value="20">20</option>
+	<option value="40">40</option>
+</Select>
--- src/routes/foo/[endpoint]/+page.svelte
+++ src/routes/foo/[endpoint]/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import { Hr, P } from 'flowbite-svelte';
 	import NewTableNav from '$lib/components/NewTableNav.svelte';
+	import TableSize from '$lib/components/TableSize.svelte';
 	let { data } = $props();
 </script>
 
 <NewTableNav {data}></NewTableNav>
 <P>{data.payload.count}</P>
+<TableSize></TableSize>
 <Hr />
 
 <ul>
